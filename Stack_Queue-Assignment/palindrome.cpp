3a55b0836a625a39024eb93def815241e7973a3c
Lee Sutton <leesutton1@gmail.com>
2016-06-15 11:20:52 -0700

    Created class template for stack and began testing in main function

--- Stack_Queue-Assignment/palindrome.cpp
+++ Stack_Queue-Assignment/palindrome.cpp
@@ -1,2 +1,20 @@
-#include <stdio>
-#include <>
+/*
+Question:
+=========
+A palindrome is a string that reads the same forwards and backwards. Using only
+a fixed number of stacks and queues, write an algorithm to determine if a string
+is a palindrome
+*/
+
+#include <iostream>
+#include "stack.h"
+
+using namespace std;
+
+int main()
+{
+  cout << "Testing Compilation" << endl;
+  // Testing the Constructor
+  Stack<char> TestStack(10);
+  return 0;
+}
--- Stack_Queue-Assignment/stack.h
+++ Stack_Queue-Assignment/stack.h
@@ -1,5 +1,21 @@
-template <typename T> class Stack
+// Create a stack class template to use in the palindrome algorithm
+// The Stack implementation will use an array
+// Author Lee Sutton
+template <typename E> class Stack
 {
+private:
+  int top;
+  int maxSize;
+  E *listarray;
+
 public:
+  // Constructor
+  Stack(int size)
+  {
+    maxSize = size;
+    top = 0;
+    listarray = new E[size];
+  };
+
 
 };
